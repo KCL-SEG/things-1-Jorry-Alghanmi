20905916b23ddb317f5303e351b7a3852a92b914
Jorry-Alghanmi <127518285+Jorry-Alghanmi@users.noreply.github.com>
2023-10-19 19:19:22 +0100

    Add a basic things model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+class Thing(Model):
+    def __init__(self, name, description, quantity):
+        self.name = name  # Attribute for first name
+        self.description = description  # Attribute for last name
+        self.quantity = quantity
+
+
+
+
